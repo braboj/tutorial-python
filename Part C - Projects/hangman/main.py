a1999543a2c5f62d28e9b38fa265e79ad6deb6a9
braboj <66906831+braboj@users.noreply.github.com>
2022-11-22 15:50:26 +0200

    Update tutorial

--- Part C - Projects/hangman/main.py
+++ Part C - Projects/hangman/main.py
@@ -1,105 +1,115 @@
 from six.moves import input
 
 
-class AppError(Exception):
+####################################################################################################
+# ERRORS
+####################################################################################################
+
+class GameError(Exception):
     pass
 
 
+####################################################################################################
+# GAME CLASSES
+####################################################################################################
+
+# TODO: Add abstract class
+class GameSession(object):
+
+    def __init__(self):
+        self.quit = False
+        self.show_guessed = False
+        self.player_name = 'Nemo'
+        self.difficulty = 0
+        self.attempts = 10
+        self.category = 'animals'
+        self.score = 0
+        self.word = []
+        self.guess = []
+
+    @classmethod
+    def from_json(cls, player_name):
+        # TODO: Read session from JSON file using player name
+        return cls()
+
+
+# TODO: Add abstract class
 class GameInput(object):
 
-    def __init__(self, parser):
-        self.parser = parser
-        self.event = ""
+    def __init__(self, session):
+        self.session = session
 
     def wait(self):
-        self.event = input('> ')
-        return self
 
+        user_input = input('> ')
 
-class GameLogic(object):
+        if user_input == "q":
+            raise GameError("Game End")
 
-    def __init__(self, player, difficulty, attempts, category):
-        self.player = player
-        self.difficulty = difficulty
-        self.attempts = attempts
-        self.category = category
-        self.score = 0
-        self.show_guessed = False
+        elif user_input == 'd':
+            self.session.difficulty += 1
 
-    def update(self, user_input):
+        elif user_input == 'a':
+            self.session.attempts += 1
 
-        if user_input.event == "quit":
-            raise AppError("Quit game")
+        elif user_input == 'c':
+            self.session.category = 'cars'
 
-        elif user_input.event == "show_guess":
-            self.show_guessed = True
-
-        else:
-            self.score += 1
+        elif user_input == "s":
+            self.session.show_guessed = True
 
         return self
 
 
+# TODO: Add abstract class
+class GameEngine(object):
+
+    def __init__(self, session):
+        self.session = session
+
+    def update(self):
+        self.session.score += 1
+        return self
+
+
+# TODO: Add abstract class
 class GameOutput(object):
 
-    def __init__(self, show_guessed=True):
-        self.show_guessed = show_guessed
+    def __init__(self, session):
+        self.session = session
 
-    @staticmethod
-    def show(state):
+    def show(self):
+        print(vars(self.session))
 
-        for parameter in vars(state):
-            print(parameter, getattr(state, parameter))
 
+####################################################################################################
+# GAME APP : Make different versions depending on input, output and engine
+####################################################################################################
 
 class GameApp(object):
 
     def __init__(self,
-                 game_input,            # Class used to read the user input
-                 game_output,           # Class used to generate the user output
-                 game_logic,            # Class used to calculate the game state
-                 use_cyrillic=False,    # Input parameter
-                 difficulty=1,          # Logic parameter
-                 attempts=20,           # Logic parameter
-                 category="birds",      # Logic parameter
-                 player="NEMO",         # Logic parameter
-                 show_guessed=False,    # Output parameter
+                 session: GameSession = None  # Session used to store the player details
                  ):
 
-        # Use the given class, create and configure input object
-        self.game_input = game_input(parser=None)
+        # Create game session
+        if not session:
+            session = GameSession()
 
-        # Use the given class, create and configure output object
-        self.game_output = game_output(
-            show_guessed=show_guessed
-        )
-
-        # Use the given class, create and configure game logic object
-        self.game_logic = game_logic(
-            player,
-            difficulty,
-            attempts,
-            category,
-        )
+        self.game_input = GameInput(session)
+        self.game_engine = GameEngine(session)
+        self.game_output = GameOutput(session)
 
     def run(self):
         while True:
             try:
-                event = self.game_input.wait()
-                state = self.game_logic.update(event)
-                self.game_output.show(state)
-            except AppError as e:
+                self.game_input.wait()
+                self.game_engine.update()
+                self.game_output.show()
+            except GameError:
                 break
 
 
 if __name__ == "__main__":
-
-    # Configure the game
-    game = GameApp(
-        game_input=GameInput,
-        game_logic=GameLogic,
-        game_output=GameOutput
-    )
-
-    # Start the game
+    game = GameApp()
     game.run()
