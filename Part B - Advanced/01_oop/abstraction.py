b15c6e55ca103a4d7d7d40348fe5734bca204e02
braboj <66906831+braboj@users.noreply.github.com>
2023-08-24 18:37:16 +0300

    Update tutorial

---
+++ Part B - Advanced/01_oop/abstraction.py
@@ -0,0 +1,24 @@
+# Example: OOP - Abstraction
+
+class Person(object):
+
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
+
+class Employee(Person):
+
+    def __init__(self, name, id_number):
+        super(Employee, self).__init__(name)
+        self.id_number = id_number
+
+    def get_id_number(self):
+        return self.id_number
+
+
+e = Employee("John", 1234)
+print(e.get_name())
+print(e.get_id_number())
