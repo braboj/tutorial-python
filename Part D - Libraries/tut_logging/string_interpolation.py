da8ae358914c99224e88eaeb508715f0abe3b8cd
braboj <66906831+braboj@users.noreply.github.com>
2023-09-09 01:12:08 +0300

    Update Part D

--- Part D - Libraries/tut_logging/string_interpolation.py
+++ Part D - Libraries/tut_logging/string_interpolation.py
@@ -3,5 +3,6 @@ import logging
 hello = 'Hello world!'
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(threadName)s - %(message)s')
-logging.warning(f'Logger message {hello}')
+root = logging.getLogger()
+root.warning(f'Logger message {hello}')
 
