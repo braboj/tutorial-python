6248f4f02749ad4165df62a2704718f06ea055f7
braboj <66906831+braboj@users.noreply.github.com>
2023-09-12 12:52:41 +0300

    Update Part B

--- Part B - Advanced/02_oop/examples/abstraction.py
+++ Part B - Advanced/02_oop/examples/abstraction.py
@@ -6,10 +6,28 @@ from abc import ABC, abstractmethod
 class PersonAbc(ABC):
     """ The abstract base class for a person answers what a person shall be able to do. """
 
+    def __init__(self):
+
+        # WHAT DOES THE PERSON HAVE?
+
+        self.name = 'Bob'
+        self.age = 42
+        self.weight = 80
+        self.height = 180
+
+    # WHAT DOES THE PERSON DO?
+
     @abstractmethod
-    def get_name(self):
+    def walk(self):
+        # Still abstract, because we don't know how a person walks.
         pass
 
     @abstractmethod
-    def get_age(self):
+    def talk(self):
+        # Still abstract, because we don't know how a person talks.
+        pass
+
+    @abstractmethod
+    def eats(self):
+        # Still abstract, because we don't know how a person eats.
         pass
