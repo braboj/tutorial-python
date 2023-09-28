7608ba44b23d89129079e5604ba07b941806c8a6
braboj <66906831+braboj@users.noreply.github.com>
2023-09-28 12:03:51 +0300

    (#46) - Update Part B with some code examples

--- Part B - Advanced/04_patterns/examples/adapter_with_inheritance.py
+++ Part B - Advanced/04_patterns/examples/adapter_with_inheritance.py
@@ -1,10 +1,8 @@
-# Example: Adapter Pattern (Class Adapter)
-
 class ChannelV1(object):
 
     @staticmethod
     def applyConfig():
-        # method name from actual legacy code in the company
+        # method name from actual legacy code in the company (Python 2.7)
         print('Configuration method of the old device class!')
 
 
@@ -19,16 +17,18 @@ class ChannelAdapter(ChannelV1, ChannelV2):
     # Class adapter (with inheritance)
 
     def applyConfig(self):
+        # The adapter's applyConfig method calls the new class's configure method
         self.configure()
 
 
 def host_app(channel):
+    # The host_app function works with the ChannelV1 interface
     channel.applyConfig()
 
 
 if __name__ == "__main__":
-    # Original code using the old service
+    # Original code using the old service (ChannelV1)
     host_app(channel=ChannelV1())
 
-    # Use adapter for the new service adapted to the old interface
+    # Use adapter for the new service (ChannelV2) adapted to the old interface (ChannelV1)
     host_app(channel=ChannelAdapter())
