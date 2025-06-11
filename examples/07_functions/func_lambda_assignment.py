c78d03b52a179c7a10e8761c6f3d3824fb971b5a
Branimir Georgiev <66906831+braboj@users.noreply.github.com>
2025-06-11 08:58:33 +0300

    Refine lambda assignment header

--- examples/07_functions/func_lambda_assignment.py
+++ examples/07_functions/func_lambda_assignment.py
@@ -1,2 +1,6 @@
+# Assigning a lambda expression to a variable
+# --------------------------------------------------------------------------------
+# Lambda expressions can be assigned to variables to create small, unnamed functions on the fly.
+
 nop = lambda: None
 print(nop())
